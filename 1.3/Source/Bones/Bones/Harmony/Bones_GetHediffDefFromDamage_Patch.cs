362180c761257c3487ff06bed6217719bcdf39b8
Steve <stevenzybertusn@gmail.com>
2022-01-09 18:47:29 -0500

    c# stuffs

---
+++ 1.3/Source/Bones/Bones/Harmony/Bones_GetHediffDefFromDamage_Patch.cs
@@ -0,0 +1,20 @@
+﻿using HarmonyLib;
+using Verse;
+using System.Collections.Generic;
+
+namespace Bones
+{
+    [HarmonyPatch(typeof(HealthUtility), "GetHediffDefFromDamage")]
+    public static class Bones_GetHediffDefFromDamage_Patch
+    {
+        [HarmonyPostfix]
+        public static void GetHediffDefFromDamage(DamageDef dam, Pawn pawn, BodyPartRecord part, ref HediffDef __result)
+        {
+            var list = new List<HediffDef> { HediffDef.Named("SZ_Stable_Fracture"), HediffDef.Named("SZ_Compound_Fracture"), HediffDef.Named("SZ_Transverse_Fracture"), HediffDef.Named("SZ_Oblique_Fracture"), HediffDef.Named("SZ_Comminuted_Fracture") };
+            if ((__result == HediffDef.Named("Crack")) && (part.def.IsSolid(part, pawn.health.hediffSet.hediffs) && (dam.hediffSolid != null)))
+            {
+                __result = list.RandomElement();
+            }
+        }
+    }
+}
---
+++ 1.3/Source/Bones/Bones/Harmony/HarmonyInstance.cs
@@ -0,0 +1,16 @@
+﻿using HarmonyLib;
+using System.Reflection;
+using Verse;
+
+namespace Bones
+{
+    [StaticConstructorOnStartup]
+    public class Main
+    {
+        static Main()
+        {
+            var harmony = new Harmony("com.bones");
+            harmony.PatchAll(Assembly.GetExecutingAssembly());
+        }
+    }
+}
