444f9afdf0f741282e0030a6abd52de86392ca42
Steve <stevenzybertusn@gmail.com>
2022-01-09 21:06:03 -0500

    updates

--- 1.3/Source/Bones/Bones/Harmony/Bones_GetHediffDefFromDamage_Patch.cs
+++ 1.3/Source/Bones/Bones/Harmony/Bones_GetHediffDefFromDamage_Patch.cs
@@ -10,10 +10,11 @@ namespace Bones
         [HarmonyPostfix]
         public static void GetHediffDefFromDamage(DamageDef dam, Pawn pawn, BodyPartRecord part, ref HediffDef __result)
         {
-            var list = new List<HediffDef> { HediffDef.Named("SZ_Stable_Fracture"), HediffDef.Named("SZ_Compound_Fracture"), HediffDef.Named("SZ_Transverse_Fracture"), HediffDef.Named("SZ_Oblique_Fracture"), HediffDef.Named("SZ_Comminuted_Fracture") };
+            var injuryList = new List<HediffDef> { HediffDef.Named("SZ_Stable_Fracture"), HediffDef.Named("SZ_Compound_Fracture"), HediffDef.Named("SZ_Transverse_Fracture"), HediffDef.Named("SZ_Oblique_Fracture"), HediffDef.Named("SZ_Comminuted_Fracture") };
+
             if ((__result == HediffDef.Named("Crack")) && (part.def.IsSolid(part, pawn.health.hediffSet.hediffs) && (dam.hediffSolid != null)))
             {
-                __result = list.RandomElement();
+                __result = injuryList.RandomElement();
             }
         }
     }
