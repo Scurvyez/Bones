c8cb9ad787da0487eddfbc382e21598b68e37d14
Steve <stevenzybertusn@gmail.com>
2022-01-12 00:50:32 -0500

    stuffz

--- 1.3/Source/Bones/Bones/Harmony/Bones_GetHediffDefFromDamage_Patch.cs
+++ 1.3/Source/Bones/Bones/Harmony/Bones_GetHediffDefFromDamage_Patch.cs
@@ -11,7 +11,7 @@ namespace Bones
         public static void GetHediffDefFromDamage(DamageDef dam, Pawn pawn, BodyPartRecord part, ref HediffDef __result)
         {
             var injuryList = new List<HediffDef> { HediffDef.Named("SZ_Stable_Fracture"), HediffDef.Named("SZ_Compound_Fracture"), HediffDef.Named("SZ_Transverse_Fracture"), HediffDef.Named("SZ_Oblique_Fracture"), HediffDef.Named("SZ_Comminuted_Fracture") };
-            //var missingBoneLabel = HediffDef.Named("SZ_MissingBone");
+            //var missingBoneLabel = HediffDef.Named("MissingBodyPart");
 
             if ((__result == HediffDef.Named("Crack")) && (part.def.IsSolid(part, pawn.health.hediffSet.hediffs) && (dam.hediffSolid != null)))
             {
