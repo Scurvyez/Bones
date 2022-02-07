53cd1855442af7dcf3d1f8e509ecd1a173a2b820
Steve <stevenzybertusn@gmail.com>
2022-02-07 01:48:16 -0500

    New colors!
    Fixed and finalized the Hediff_Fracture code for color=coded hediff text.
    
    Changed finger and toe bones to MovingLimbDigit and ManipulationLimbDigit.

--- 1.3/Source/Bones/Bones/Hediffs/Hediff_Fracture.cs
+++ 1.3/Source/Bones/Bones/Hediffs/Hediff_Fracture.cs
@@ -1,33 +1,38 @@
-﻿using Verse;
-using UnityEngine;
+﻿using UnityEngine;
+using Verse;
 
 namespace Bones
 {
     public class Hediff_Fracture : Hediff_Injury
     {
         private static readonly Color PermanentInjuryColor = new(0.72f, 0.72f, 0.72f);
-        public override Color LabelColor => this.IsPermanent() ? PermanentInjuryColor : Color.Lerp(Color.red, Color.green, ColorSeverity());
+        public override Color LabelColor => this.IsPermanent() ? PermanentInjuryColor : Color.Lerp(Color.cyan, Color.magenta, ColorSeverity());
 
         public float ColorSeverity()
         {
-            float efficiency = PawnCapacityUtility.CalculatePartEfficiency(pawn.health.hediffSet, Part);
+            float eff = PawnCapacityUtility.CalculatePartEfficiency(pawn.health.hediffSet, Part);
+            float effPer = ((eff) / 1) * 100;
 
-            if ((efficiency <= (100 / efficiency) * 100) && (efficiency >= (76 / efficiency) * 100))
-            {
-                return 0.88f;
-            }
-            else if ((efficiency <= (75 / efficiency) * 100) && (efficiency >= (51 / efficiency) * 100))
-            {
-                return 0.63f;
-            }
-            else if ((efficiency <= (50 / efficiency) * 100) && (efficiency >= (26 / efficiency) * 100))
-            {
-                return 0.38f;
-            }
+            if (effPer <= 100 && effPer >= 91)
+                return 0.955f;
+            else if (effPer <= 90 && effPer >= 81)
+                return 0.855f;
+            else if (effPer <= 80 && effPer >= 71)
+                return 0.755f;
+            else if (effPer <= 70 && effPer >= 61)
+                return 0.655f;
+            else if (effPer <= 60 && effPer >= 51)
+                return 0.555f;
+            else if (effPer <= 50 && effPer >= 41)
+                return 0.455f;
+            else if (effPer <= 40 && effPer >= 31)
+                return 0.355f;
+            else if (effPer <= 30 && effPer >= 21)
+                return 0.255f;
+            else if (effPer <= 20 && effPer >= 11)
+                return 0.155f;
             else
-            {
-                return 0.13f;
-            }
+                return 0.055f;
         }
     }
 }
