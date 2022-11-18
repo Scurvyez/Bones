f5a984b6002023d0405e4fb82f724ecd023d8b5f
Steve <stevenzybertusn@gmail.com>
2022-11-18 16:51:04 -0500

    1.4 update :)

---
+++ 1.4/Source/Bones/Bones/HediffGivers/HediffGiver_BoneFractureAtOldAge.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using RimWorld;
+using RimWorld.Planet;
+using Verse;
+
+namespace Bones
+{
+    public class HediffGiver_BoneFractureAtOldAge : HediffGiver_RandomAgeCurved
+	{
+		public float mtbDays;
+
+		public override void OnIntervalPassed(Pawn pawn, Hediff cause)
+		{
+			float num = mtbDays;
+			float num2 = ChanceFactor(pawn);
+			if (num2 != 0f && Rand.MTBEventOccurs(num / num2, 60000f, 60f) && TryApply(pawn))
+			{
+				SendLetter(pawn, cause);
+			}
+		}
+    }
+}
